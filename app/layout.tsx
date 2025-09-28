ccf213d3f50df8cebce19280b8fa77310c5265f1
AlvinDHacker <alvindsouza2204@gmail.com>
2025-09-28 11:40:21 +0530

    Fixed it

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { HeroHeader } from "@/components/Header";
 import FooterSection from "@/components/Footer";
+import ChatComponent from "@/components/chat/ChatComponent";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <HeroHeader />
         {children}
         <FooterSection />
+        <ChatComponent projectId="1" userId="1" receiverId="2" />
       </body>
     </html>
   );
